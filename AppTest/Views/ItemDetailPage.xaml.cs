2785e7fcf08779e8f9023d6e50412ef2689d03cf
A.W <71868337+AImperator@users.noreply.github.com>
2023-08-13 00:35:31 +0200

    Projektdateien hinzufügen.

---
+++ AppTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AppTest.ViewModels;
+
+namespace AppTest.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
